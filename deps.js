857b81a3c601cd98dcb64c29dc8bcd3b69e4da80
Oscar Otero <oom@oscarotero.com>
2022-06-04 17:14:14 +0200

    updated deps urls to be more deno-friendly

--- deps.js
+++ deps.js
@@ -1,10 +1,10 @@
 export * as pico from "https://deno.land/std@0.141.0/fmt/colors.ts";
-export { default as browserslist } from "https://esm.sh/browserslist@4.20.3/";
+export { default as browserslist } from "https://esm.sh/browserslist@4.20.3/index.js";
 export { default as parser } from "https://deno.land/x/postcss_value_parser@4.2.0/mod.js";
 import caniuse_lite from "../data/caniuse-lite.js";
 
-export { default as range } from "https://esm.sh/normalize-range@0.1.2";
-export { default as FractionJs } from "https://esm.sh/fraction.js@4.2.0/";
+export { default as range } from "https://esm.sh/normalize-range@0.1.2/index.js";
+export { default as FractionJs } from "https://esm.sh/fraction.js@4.2.0/fraction.js";
 
 import postcss from "https://deno.land/x/postcss@8.4.13/mod.js";
 const { list, parse } = postcss;
